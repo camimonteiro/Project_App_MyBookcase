68b0180fa0f8441c1960c32aaf93b86c0c5ff9a0
Camille Monteiro <camillemonteiro.dev@gmail.com>
2022-03-24 21:18:44 -0300

    feat: start of modeling the book database
    BREAKING CHANGE:

---
+++ Project_App_MyBookcase/Models/BookRegistration.swift
@@ -0,0 +1,18 @@
+//
+//  BookRegistration.swift
+//  Project_App_MyBookcase
+//
+//  Created by Cami on 24/03/22.
+//
+
+import Foundation
+
+struct BookRegistration:
+    var id: Int
+    var title: String
+    var author: String
+    var category: String
+    var ratingStars: Int
+    var ratingComents: String
+    var observations: String
+    
