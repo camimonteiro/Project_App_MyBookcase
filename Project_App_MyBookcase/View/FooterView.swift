a3816d2ae0fa9f61107f9bb2deed269a4e8b464c
Camille Monteiro <camillemonteiro.dev@gmail.com>
2022-03-26 17:14:13 -0300

    style: footer menu done

--- Project_App_MyBookcase/View/FooterView.swift
+++ Project_App_MyBookcase/View/FooterView.swift
@@ -9,12 +9,59 @@ import SwiftUI
 
 struct FooterView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { view in
+            VStack {
+                HStack {
+                    Image("estrela-meio-vazia")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 50, height: 50)
+                        .offset(x: 50)
+                        
+                    Spacer()
+                    Image("coracao")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 50, height: 50)
+                        .offset(x:5)
+                    Spacer()
+                    Image("do-utilizador-2")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 50, height: 50)
+                        .offset(x: -50)
+                }
+                .frame(width: view.size.width, height: 80, alignment: .center)
+                .offset(y:5)
+                                
+                HStack {
+                    Text("Minhas Avaliações")
+                        .font(.custom("Avenir light", size: 13))
+                        .foregroundColor(.white)
+                        .offset(x: 20)
+                    Spacer()
+                    Text("Próximos Livros")
+                        .font(.custom("Avenir light", size: 13))
+                        .foregroundColor(.white)
+                        .offset(x:-15)
+                    Spacer()
+                    Text("Meu Perfil")
+                        .font(.custom("Avenir light", size: 13))
+                        .foregroundColor(.white)
+                        .offset(x: -40)
+                }
+                .frame(width: view.size.width, height: 20, alignment: .center)
+                .offset(y:-10)
+            }
+            .frame(width: view.size.width, height: 100, alignment: .bottom)
+            .background(.blue)
+        }
     }
 }
 
 struct FooterView_Previews: PreviewProvider {
     static var previews: some View {
         FooterView()
+            .previewLayout(.fixed(width: 400, height: 100))
     }
 }
